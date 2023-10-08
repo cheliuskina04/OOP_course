eb403883b4fd6e369a6e9511834d8873060cd580
cheliuskina04 <122870686+cheliuskina04@users.noreply.github.com>
2023-10-08 13:58:48 +0300

    step 7

--- lab1/src/main/java/StringCalculator.java
+++ lab1/src/main/java/StringCalculator.java
@@ -3,6 +3,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
@@ -14,50 +15,37 @@ public class StringCalculator {
 
         if (text.isEmpty()) {  //empty string
             return 0;
-
         } else { //string is not empty
+            String[] split_strings;
             List<Integer> all_nums = new ArrayList<Integer>();  //list of nums to add
 
             if (text.startsWith("//")) {
-                Matcher match_text = Pattern.compile("//(.)\n(.*)", Pattern.DOTALL).matcher(text);
-                match_text.find();
+                Matcher match_text = Pattern.compile("//\\[(.+)]\n(.*)", Pattern.DOTALL).matcher(text);
+                match_text.matches();
                 String my_delimiter = match_text.group(1);
-                String[] half_split_strings = match_text.group(2).split(my_delimiter);
-
-                for (int i = 0; i < half_split_strings.length; i++) {
-                    text = half_split_strings[i];
-                    String[] split_strings = text.split("[,\n]");
-                    try {
-                        for (int j = 0; j < split_strings.length; j++)
-                            all_nums.add(j, Integer.parseInt(split_strings[j]));
-                    } catch (NumberFormatException ex) {
-                        System.out.println("ERROR: There is smth except except numbers or odd delimiters.");
-                        exit(1);
-                    }
-                }
-
-
-            } else {
-                String[] split_strings = text.split("[,\n]");
-                try {
-                    for (int j = 0; j < split_strings.length; j++)
-                        all_nums.add(j, Integer.parseInt(split_strings[j]));
-                } catch (NumberFormatException ex) {
-                    System.out.println("ERROR: There is smth except except numbers or odd delimiters.");
-                    return -1;
-                }
+                split_strings = match_text.group(2).replace(my_delimiter, ",").split("[,\n]");
             }
+             else {
+                split_strings = text.split("[,\n]");
+            }
+            try {
+                for (int j = 0; j < split_strings.length; j++)
+                    all_nums.add(j, Integer.parseInt(split_strings[j]));
+            } catch (NumberFormatException ex) {
+                System.out.println("ERROR: There is smth except except numbers or odd delimiters.");
+                return -1;
+            }
+
             List<Integer> negative_nums = new ArrayList<>();
-            for(int i = 0; i<all_nums.toArray().length; i++)
-                if(all_nums.get(i)<0)
+            for (int i = 0; i < all_nums.toArray().length; i++)
+                if (all_nums.get(i) < 0)
                     negative_nums.add(all_nums.get(i));
 
 
-
-            if (negative_nums.toArray().length >0)
+            if (negative_nums.toArray().length > 0)
                 throw new RuntimeException("ERROR: There are negative numbers: " + join(",", Arrays.toString(negative_nums.toArray())));
             else
-                 return all_nums.stream().mapToInt(i -> i).filter(i -> i<=1000).sum();
+                return all_nums.stream().mapToInt(i -> i).filter(i -> i <= 1000).sum();
         }
     }
 }
