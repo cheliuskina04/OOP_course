bb5e54ab79d437db65b9beebdd28919dcd608b61
cheliuskina04 <122870686+cheliuskina04@users.noreply.github.com>
2023-10-14 13:03:26 +0300

    Update StringCalculator.java

--- lab1/src/main/java/StringCalculator.java
+++ lab1/src/main/java/StringCalculator.java
@@ -12,11 +12,8 @@ import static java.lang.System.exit;
 
 public class StringCalculator {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter string: ");
-
-        String mystring = sc.nextLine();
-        System.out.println(add(mystring));
+        String mystring = "1,,2";
+        System.out.println("Result: " + add(mystring));
     }
     public static int add(String text) {
 
